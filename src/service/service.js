2743a53ba3c4229d79f508b0cbc63b9fa154c46d
Krystle <krystleweinrich@yahoo.com>
2016-03-07 07:48:01 -0800

    added apply to position button
    apply button will be in a service listing if user is not the employer.
    Currently the button leads nowhere. Need to add a new page, need to add
    notification for employee.

--- src/service/service.js
+++ src/service/service.js
@@ -4,6 +4,113 @@ Services = new Mongo.Collection("services");
 FlowRouter.route('/gig/:id', {
 	action: function(params) {
 		var service = Services.findOne({_id: params.id});
+        console.log("Flowrouter.route");
+        
+		console.log(service);
+        //Will either bring in the service or have an empty one so there are no errors when bringing up service.employer
+        service = service || {}
+        //used for displaying the apply button for employees (see servicelistingpage.html)
+        service.isUserEmployer = service.employer === Meteor.userId();
+
+        //debug logs
+        console.log(this);
+        console.log(Meteor.userId());
+        
+		BlazeLayout.render("layout", {
+			area: "serviceListingPage",
+			params: params,
+			service: service
+		});
+	}
+});
+
+FlowRouter.route('/postgig', {
+	action: function(params) {
+		BlazeLayout.render("layout", {area: "servicePostForm"});
+	}
+});
+
+if (Meteor.isServer) {
+
+	Meteor.publish("services", function () {
+		return Services.find({});
+	});
+
+	Meteor.methods({
+		"createService": function(serviceObj) {
+			
+            if (!this.userId) {
+				console.log("not logged in");
+				return false;
+			}
+
+			if (!serviceObj) {
+				console.log("no service obj");
+				return false;
+			}
+
+			if (typeof(serviceObj.title) != "string" || serviceObj.title.length == 0) {
+				console.log("no title");
+				return false;
+			}
+
+			if (typeof(serviceObj.description) != "string" || serviceObj.description.length == 0) {
+				console.log("no descript");
+				return false;
+			}
+
+			if ( Number.isNaN(parseInt(serviceObj.wage, 10)) ) {
+				console.log("no wage");
+				return false;
+			}
+
+			var newService = Services.insert({
+				employer: this.userId,
+				title: serviceObj.title,
+				description: serviceObj.description,
+				wage: serviceObj.wage,
+			});
+
+			return newService;
+		}
+	});
+
+}
+
+if (Meteor.isClient) {
+
+	Meteor.subscribe("services");
+
+	Template.servicePostForm.events({
+		"input #serviceWage": function(event) {
+			event.target.value = event.target.value.replace(/\D/g, "");
+		},
+
+		"submit .servicePostForm": function() {
+			event.preventDefault();
+
+			Meteor.call("createService", {
+				title: event.target.serviceTitle.value,
+				description: event.target.serviceDescription.value,
+				wage: parseFloat(event.target.serviceWage.value)
+			}, function(err, id) {
+				if (id) {
+					FlowRouter.go("/gig/" + id);
+				} else {
+					console.log("Failure! Handle this!");
+				}
+			});
+
+		}
+	});
+
+}
+/*
+Services = new Mongo.Collection("services");
+
+FlowRouter.route('/gig/:id', {
+	action: function(params) {
+		var service = Services.findOne({_id: params.id});
 
 		console.log(service);
 
@@ -95,3 +202,4 @@ if (Meteor.isClient) {
 	});
 
 }
+*/
