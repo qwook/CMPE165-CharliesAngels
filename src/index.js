77692becdf3e5d1e174a3b70a33b317a1893a600
Henry Tran <hen@Henrys-MacBook-Pro.local>
2016-04-16 04:02:52 -0700

    Clean up styling of gigsearch

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ FlowRouter.route('/', {
 });
 
 if (Meteor.isClient) {
+
+    Meteor.startup(function() {
+        GoogleMaps.load();
+    });
+
     Template.layout.events({
         "click .post-a-gig": function (event) {
         },
@@ -33,6 +38,34 @@ if (Meteor.isClient) {
             });
             return mapped;
         },
+        "mapOptions": function() {
+            // Make sure the maps API has loaded
+            if (GoogleMaps.loaded()) {
+                // Map initialization options
+                return {
+                    streetViewControl: false, // hide the yellow Street View pegman
+                    scaleControl: false, // allow users to zoom the Google Map
+                    center: new google.maps.LatLng(37.3382, -121.8863),
+                    disableDefaultUI: true,
+                    draggable: false,
+                    scrollwheel: false,
+                    panControl: false,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP,
+                    zoom: 16,
+                    styles: [
+                        {
+                            "stylers": [
+                            { "hue": "#002bff" },
+                            { "invert_lightness": true },
+                            { "saturation": -63 },
+                            { "lightness": 30 },
+                            { "gamma": 0.54 }
+                            ]
+                        }
+                    ]
+                };
+            }
+        }
     });
 
     Template.layout.helpers({
